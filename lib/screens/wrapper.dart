036e4e616c7fa7d21d7f4dfb7b6ff773b61b26d0
Merlijn <merlijnvanlent@gmail.com>
2020-09-13 18:39:42 +0200

    Firebase anon

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,9 @@
+import 'package:de_koffie_manager/screens/authenticate/authenticate.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Authenticate();
+  }
+}
